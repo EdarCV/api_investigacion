144f88405edfa4b9741eb2dcaef654b0b5098b5c
Edar Cervantes <ecervantesv15@unc.edu.pe>
2021-10-08 00:41:31 -0500

    Terminando autenticación y acceso de servicio

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express'); //Sintaxis para importar modulos en nodejs
+
 require('dotenv').config();
 const { dbConection } = require('./config/db');
 
 const cors = require('cors');
+
 
 //Crear el servidor express
 const app = express();
@@ -10,20 +12,22 @@ const app = express();
 //Estableciendo la config de cors
 app.use(cors());
 
+app.use(express.json());
+
+
 //Crear la conexion a la bd
 dbConection();
 
 //Verificando variables de entorno
 //console.log(process.env);
 
-app.get('/', (req, res) => {
-    res.status(400).json({
-        ok: true,
-        msg: 'Bienvenidos a node'
-    });
-});
+//Rutas de la API Proyectos
+app.use('/api/usuarios', require('./routes/usuario.routes'));
+app.use('/api/login', require('./routes/auth.routes'));
+//app.use('/api/investigadores', require('./routes/investigadoresRoutes'));
 
-//process.env.PORT
+
+//Codigo para desplegar servidor
 app.listen(process.env.PORT, () => {
     console.log('Servidor Nodejs desplegado en el puerto: ' + process.env.PORT)
 })
